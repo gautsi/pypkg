9441b31af984bb642083868eccd4adf03aec81dd
Gautam Sisodia <gautam.sisodia@gmail.com>
2019-12-26 03:27:43 +0000

    second commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "example-pkg-gautsi".
+    name = "example-pkg-gautsi",
     version = "0.0.1",
     author = "Gautam Sisodia",
     packages = setuptools.find_packages(),
